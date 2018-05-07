da35910df11e057fc60c6f8e12f64c56aea5e552
Patricio <pato.lussenhoff@gmail.com>
2018-05-07 19:18:51 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -13,9 +13,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://patopeth:Ihsahn657@ds217360.mlab.com:17360/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
